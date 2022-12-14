9945706d426a116eeb6a59f63fd50f895da8fc8b
Emir Çetin MEMİŞ <zeymenak@gmail.com>
2022-12-14 12:57:00 +0300

    bug fix

--- test.py
+++ test.py
@@ -11,122 +11,89 @@ def create_svg_output(number_of_cities:int):
     gen_stroke_style = "fill: rgb(255, 255, 255);"
     gen_text_style = "white-space: pre;"
 
-    if number_of_cities % 2 == 0 :
-        maxColNum = int(number_of_cities/2)
-    else :
-        maxColNum = int(number_of_cities/2) + 1
+    vertical_inpad = 6
+    horizontal_inpad = 6
 
-    if number_of_cities == 0 :
-        maxRowNum = 0
-    elif number_of_cities == 1 :
-        maxRowNum = 1
-    else :
-        maxRowNum = 2
+    max_cities_horizontally = int(number_of_cities/2) + (1 if number_of_cities % 2 == 1 else 0) 
+    max_cities_vertically = (0 if number_of_cities == 0 else 1 if number_of_cities == 1 else 2)
 
-    circR = 30
-    p = 10
-    city_locs = []
+    canvas_padding = 10
+    city_locations = []
+    city_radius = 30
 
-    width = 2*p + (maxColNum-1)*7*circR + 2*circR
-    height = 2*p + (maxRowNum-1)*8*circR + 2*circR
+    canvas_width = 2*canvas_padding + (max_cities_horizontally-1)*vertical_inpad*city_radius + 2*city_radius
+    canvas_height = 2*canvas_padding + (max_cities_vertically-1)*horizontal_inpad*city_radius + 2*city_radius
 
     for i in range(number_of_cities) :
         if i == 0 :
-            x = circR+p
-            y = circR+p
-            loc = (x,y)
-            city_locs.append(loc)
+            x_location = city_radius+canvas_padding
+            y_location = city_radius+canvas_padding
+            current_location = (x_location,y_location)
+            city_locations.append(current_location)
         else :
             if i % 2 == 0 :
-                previousX = city_locs[i-2][0]
-                x = previousX + 7*circR
-                y = city_locs[i-2][1]
-                loc = (x,y)
-                city_locs.append(loc)
+                previousX = city_locations[i-2][0]
+                x_location = previousX + vertical_inpad*city_radius
+                y_location = city_locations[i-2][1]
+                current_location = (x_location,y_location)
+                city_locations.append(current_location)
             else :
-                previousX = city_locs[i-1][0]
-                x = previousX
-                y = city_locs[i-1][1] + 8*circR
-                loc = (x,y)
-                city_locs.append(loc)
+                previousX = city_locations[i-1][0]
+                x_location = previousX
+                y_location = city_locations[i-1][1] + horizontal_inpad*city_radius
+                current_location = (x_location,y_location)
+                city_locations.append(current_location)
                 
-    city_circles = ""
-    for i in city_locs :
-        str = f"    <ellipse stroke=\"black\" stroke-width=\"1\" cx=\"{i[0]}\" cy=\"{i[1]}\" rx=\"30\" ry=\"30\" style=\"fill: rgb(255, 255, 255);\"/>\n"
-        city_circles += str
+    svg_city_circles = "    <!-- City Circles -->\n"
+    for current_location in city_locations :
+        svg_city_circles += f"    <ellipse stroke=\"black\" stroke-width=\"1\" cx=\"{current_location[0]}\" cy=\"{current_location[1]}\" rx=\"30\" ry=\"30\" style=\"fill: rgb(255, 255, 255);\"/>\n"
 
-    city_labels = ""
-    count = 1
-    for i in city_locs :
-        str = f"    <text x=\"{i[0]}\" y=\"{i[1]}\" font-family=\"Times New Roman\" font-size=\"20\" style=\"white-space: pre; text-anchor: middle; dominant-baseline: middle;\">{count}</text>\n"
-        count += 1
-        city_labels += str
+    city_labels = "    <!-- City Labels -->\n"
+    for cityID, current_location in enumerate(city_locations) :
+        city_labels += f"    <text x=\"{current_location[0]}\" y=\"{current_location[1]+horizontal_inpad}\" font-family=\"Times New Roman\" font-size=\"20\" style=\"white-space: pre; text-anchor: middle; dominant-baseline: middle;\">{cityID+1}</text>\n"
 
     svgStart = "<?xml version=\"1.0\" encoding=\"utf-8\"?>"
-    svgSetting = f"<svg width=\"{width}\" height=\"{height}\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\">"
-    svgBorder = f"<rect x=\"0\" y=\"0\" width=\"{width}\" height=\"{height}\" style=\"fill: rgb(255, 255, 255); stroke: rgb(0, 0, 0); stroke-width: 1;\"/>"
+    svgSetting = f"<svg width=\"{canvas_width}\" height=\"{canvas_height}\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\">"
     svgEnd = "</svg>"
 
-    edges = ""
+    edges = "\n    <!-- Edges -->\n"
     for i in range(number_of_cities) :
         for j in range(number_of_cities) :
-            if i != j :
-                if abs(i-j) <= 3 :
-                    x1 = city_locs[i][0]
-                    y1 = city_locs[i][1]
-                    x2 = city_locs[j][0]
-                    y2 = city_locs[j][1]
-                    str = f"    <line x1=\"{x1}\" y1=\"{y1}\" x2=\"{x2}\" y2=\"{y2}\" stroke=\"black\" stroke-width=\"1\"/>\n"
-                    edges += str
+            if i != j and abs(i-j) <= 3:
+                edges += f"    <line x1=\"{city_locations[i][0]}\" y1=\"{city_locations[i][1]}\" x2=\"{city_locations[j][0]}\" y2=\"{city_locations[j][1]}\" stroke=\"black\" stroke-width=\"1\"/>\n"
 
-    edge_labels = ""
+    edge_labels = "    <!-- Edge Labels -->\n"
     for i in range(number_of_cities) :
         for j in range(number_of_cities) :
-            if i != j :
-                if abs(i-j) <= 3 :
-                    x1 = city_locs[i][0]
-                    y1 = city_locs[i][1]
-                    x2 = city_locs[j][0]
-                    y2 = city_locs[j][1]
-                    x = (x1+x2)/2
-                    y = (y1+y2)/2
-                    str = f"    <text x=\"{x}\" y=\"{y}\" font-family=\"Times New Roman\" font-size=\"20\" style=\"white-space: pre; text-anchor: middle; dominant-baseline: middle;\">{i+j+2}</text>\n"
-                    edge_labels += str
-
-    edge_label_circles = ""
+            if i != j and abs(i-j) <= 3:
+                edge_labels += f"    <text x=\"{(city_locations[i][0]+city_locations[j][0])/2}\" y=\"{(city_locations[i][1]+city_locations[j][1])/2+horizontal_inpad}\" font-family=\"Times New Roman\" font-size=\"20\" style=\"white-space: pre; text-anchor: middle; dominant-baseline: middle;\">{i+j+2}</text>\n"
+    
+    edge_label_circles = "    <!-- Edge Label Circles -->\n"
     for i in range(number_of_cities) :
         for j in range(number_of_cities) :
-            if i != j :
-                if abs(i-j) <= 3 :
-                    x1 = city_locs[i][0]
-                    y1 = city_locs[i][1]
-                    x2 = city_locs[j][0]
-                    y2 = city_locs[j][1]
-                    x = (x1+x2)/2
-                    y = (y1+y2)/2
-
-                    y -= 0.03*circR*1.2
-                    
-                    str = f"    <ellipse stroke=\"black\" stroke-width=\"1\" cx=\"{x}\" cy=\"{y}\" rx=\"15\" ry=\"15\" style=\"fill: rgb(255, 255, 255); stroke-dasharray: 2px;\"/>\n"
-                    edge_label_circles += str
+            if i != j and abs(i-j) <= 3:
+                edge_label_circles += f"    <ellipse stroke=\"black\" stroke-width=\"1\" cx=\"{((city_locations[i][0]+city_locations[j][0])/2)}\" cy=\"{((city_locations[i][1]+city_locations[j][1])/2)}\" rx=\"15\" ry=\"15\" style=\"fill: rgb(255, 255, 255); stroke-dasharray: 2px;\"/>\n"
 
     def create_svg(args) :
-        
         svg = ""
-
         for arg in args :
             svg += arg
             svg += "\n"
 
         return svg
 
-    svg = create_svg([svgStart, svgSetting, svgBorder, edges, city_circles, city_labels, edge_label_circles, edge_labels, svgEnd])
+    svg = create_svg([svgStart, svgSetting, edges, svg_city_circles, city_labels, edge_label_circles, edge_labels, svgEnd])
 
     with open("index.svg", "w") as f :
         f.write(svg)
-
 
 def colorize_svg_output():
     pass
 
 create_svg_output(10)
+
+from svglib.svglib import svg2rlg
+from reportlab.graphics import renderPDF, renderPM
+
+drawing = svg2rlg("index.svg")
+renderPM.drawToFile(drawing, "temp.png", fmt="PNG", dpi=200)
