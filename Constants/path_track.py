f0e56eff18ec6dec3a23276bf3885804554148b9
Emir Çetin MEMİŞ <zeymenak@gmail.com>
2022-12-15 22:20:29 +0300

    update pathes

--- Constants/path_track.py
+++ Constants/path_track.py
@@ -4,7 +4,13 @@ def connect_pathes(*pathes):
     return os.path.join(*pathes)
 
 SAFE_CACHED_FOLDERS = ["App", "Constants", "Utilities"]
-SAFE_PRE_EXISTING_CHECKLIST = ["Data Export"]
+SAFE_UNNECESSARY_FOLDERS = ["Temp"]
+SAFE_PRE_EXISTING_CHECKLIST = ["Data Export", "Temp"]
 
 STORE_DATA_OUTPUT_PATH = "Data Export"
+STORE_TEMP_OUTPUT_PATH = "Temp"
 
+VISUAL_DJIKSTRA_SVG_OUTPUT_PATH = connect_pathes(STORE_TEMP_OUTPUT_PATH , "Djikstra.svg")
+VISUAL_ASTAR_SVG_OUTPUT_PATH = connect_pathes(STORE_TEMP_OUTPUT_PATH , "AStar.svg")
+VISUAL_DJIKSTRA_PNG_OUTPUT_PATH = connect_pathes(STORE_DATA_OUTPUT_PATH , "Djikstra.png")
+VISUAL_ASTAR_PNG_OUTPUT_PATH = connect_pathes(STORE_DATA_OUTPUT_PATH , "AStar.png")
