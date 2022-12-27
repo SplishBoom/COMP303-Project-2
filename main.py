56b06f556c69ef66c8975d61442b87f852c1a226
Emir Çetin MEMİŞ <zeymenak@gmail.com>
2022-12-27 20:03:40 +0300

    update main for new visual output style

--- main.py
+++ main.py
@@ -2,5 +2,9 @@ from Utilities import safeStart, safeStop, visualize
 
 if __name__ == "__main__" :
     safeStart()
-    visualize(10, "Djikstra")
+    
+    path_of_node_identities = [1,2,3,4,5,6,7,8,9]
+    
+    visualize(10, "Djikstra", path_of_node_identities)
+    
     safeStop()
