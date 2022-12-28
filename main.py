a30f61c6d006a5abe0d11824e292b9f77bcab330
Emir Çetin MEMİŞ <zeymenak@gmail.com>
2022-12-28 11:08:36 +0300

    main modified for graph

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from Utilities import safeStart, safeStop, visualize
+from Algorithms.MyGraph import MyGraph
 
 if __name__ == "__main__" :
     safeStart()
-    
+    n = 10
     path_of_node_identities = [1,2,3,4,5,6,7,8,9]
     
-    visualize(10, "Djikstra", path_of_node_identities)
+    visualize(n, "Djikstra", path_of_node_identities)
     
+    objct = MyGraph(n)
+    print(objct)
+    print(objct.graph)
+
     safeStop()
