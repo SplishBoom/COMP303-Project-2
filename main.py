bec2be8a1ee7559b7086fff4815beadaa6f17522
Emir Çetin MEMİŞ <zeymenak@gmail.com>
2022-12-29 11:52:05 +0300

    modify manin for astar

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 from Utilities import safeStart, safeStop, visualize
 from Algorithms.MyGraph import MyGraph
 from Algorithms.Dijkstra import dijkstra
+from Algorithms.AStar import a_star
 
 if __name__ == "__main__" :
     safeStart()
-    n = 17
+    n = 10
     s = 3
-    d = 16
+    d = 10
 
     graph_object = MyGraph(n)
     graph = graph_object.graph
 
-    path_of_node_identities, total_cost, edge_count = dijkstra(graph, s, d)
-    
-    visualize(n, "Djikstra", path_of_node_identities)
+    path_of_node_identities, total_cost, edge_count, number_of_visited_nodes = dijkstra(graph, s, d)
+    print(path_of_node_identities, total_cost, edge_count, number_of_visited_nodes)
+    #visualize(n, "Djikstra", path_of_node_identities)
+
+    path_of_node_identities, total_cost, edge_count, number_of_visited_nodes = a_star(graph, s, d)
+    print(path_of_node_identities, total_cost, edge_count, number_of_visited_nodes)
+    #visualize(n, "AStar", path_of_node_identities)
 
     safeStop()
