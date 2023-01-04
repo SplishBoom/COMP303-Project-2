c60a6db0f37a70d9c17b026fbb94f5fac7b6488c
Emir Çetin MEMİŞ <zeymenak@gmail.com>
2023-01-04 09:30:32 +0300

    Update Dijkstra.py

--- Algorithms/Dijkstra.py
+++ Algorithms/Dijkstra.py
@@ -19,18 +19,7 @@ class Dijkstra:
         """
         Constructor of Dijkstra class.
         """
-        self.iterations = {
-            "A": 0,
-            "B": 0,
-            "C": 0,
-            "D": 0,
-            "E": 0,
-            "F": 0,
-            "G": 0,
-            "H": 0,
-            "I": 0,
-            "J": 0,
-        }
+        self.iterations = {chr(i):0 for i in range(65, 65+21)}
 
     def _dijkstra(self, graph:dict, start:int, end:int) -> tuple:
         """
@@ -40,62 +29,57 @@ class Dijkstra:
             start -> int : start node.
             end -> int : end node.
         @returns:
-            algorithm_type -> str : name of the algorithm.
             path -> list : list of nodes, that are in the shortest path in order.
             cost -> int : cost of the shortest path.
             iterations -> dict : dictionary which includes the number of iterations of each step of the algorithm.
         """
         # Initialize a min heap H and a distances array D.
-        H = []
-        D = {}
-        self.iterations["A"] += 1
+        H = []                                                                                                      ; self.iterations['A'] += 1
+        D = {}                                                                                                      ; self.iterations['B'] += 1
 
         # Insert the starting vertex into H and set its distance in D to 0.
-        heappush(H, (0, start))
-        D[start] = 0
-        self.iterations["B"] += 1
+        heappush(H, (0, start))                                                                                     ; self.iterations['C'] += 1
+        D[start] = 0                                                                                                ; self.iterations['D'] += 1
 
         # While H is not empty:
         while H:
-            self.iterations["C"] += 1
+            pass                                                                                                    ; self.iterations['E'] += 1
             # Extract the vertex v with the minimum distance from H.
-            _, v = heappop(H)
+            _, v = heappop(H)                                                                                       ; self.iterations['F'] += 1
 
             # For each neighbor w of v:
             for w in graph[v]:
-                self.iterations["D"] += 1
+                pass                                                                                                ; self.iterations['G'] += 1
                 # Calculate the distance from the starting vertex to w through v.
-                d = D[v] + graph[v][w]
+                d = D[v] + graph[v][w]                                                                              ; self.iterations['H'] += 1
 
                 # If this distance is less than the current distance in D for w, update the distance in D for w.
                 if w not in D or d < D[w]:
-                    self.iterations["E"] += 1
-                    D[w] = d
+                    pass                                                                                            ; self.iterations['I'] += 1
+                    D[w] = d                                                                                        ; self.iterations['J'] += 1
 
                     # If w is not in H, add it to H.
-                    heappush(H, (d, w))
+                    heappush(H, (d, w))                                                                             ; self.iterations['K'] += 1
         
         # The distances array D now contains the shortest distances from the starting vertex to all other vertices.
         # By backtracking from the end vertex, we can find the shortest path.
-        self.iterations["F"] += 1
-        path = []
-        cost = D[end]
+        path = []                                                                                                   ; self.iterations['L'] += 1
+        cost = D[end]                                                                                               ; self.iterations['M'] += 1
         while end != start:
-            self.iterations["G"] += 1
-            path.append(end)
+            pass                                                                                                    ; self.iterations['N'] += 1
+            path.append(end)                                                                                        ; self.iterations['O'] += 1
             for node in graph:
-                self.iterations["H"] += 1
+                pass                                                                                                ; self.iterations['P'] += 1
                 if end in graph[node] and D[node] + graph[node][end] == D[end]:
-                    self.iterations["I"] += 1
-                    end = node
+                    pass                                                                                            ; self.iterations['Q'] += 1
+                    end = node                                                                                      ; self.iterations['R'] += 1
                     break
-        self.iterations["J"] += 1
-        path.append(start)
-        path.reverse()
+        path.append(start)                                                                                          ; self.iterations['S'] += 1
+        path.reverse()                                                                                              ; self.iterations['T'] += 1
 
         # Return values.
+        pass                                                                                                        ; self.iterations['U'] += 1
         return (
-            "Djikstra", 
             path, 
             cost,
             self.iterations
